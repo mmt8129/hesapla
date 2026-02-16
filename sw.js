67d775545b613fda998fa45c41b08d7baaf4b89f
mmt8129 <06mmt8129@gmail.com>
2026-02-16 21:02:32 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,20 @@
+const CACHE_NAME = 'hesap-v1';
+const urlsToCache = [
+  'index.html',
+  'manifest.json',
+  'icon.png'
+];
 
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
